0c1a4d248355d5536eb1b419a70b03e583fa67ea
hakob-yan <hakob9735@gmail.com>
2022-06-28 18:43:02 +0400

    regExp

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from "react"
 import { Slider } from "./Slider/Slider"
 
 export const App = () => {
-    const [price, setPrice] = useState<number>(30);
+    const [price, setPrice] = useState<number>(1);
 
     return (
         <Slider
             price={price}
             setPrice={setPrice}
-            min={30}
-            max={400}
+            min={1}
+            max={5}
             currency='$'
         />
     );
--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 import * as S from './SliderStyled';
-import { Thumb, Track } from "./utils"
+import { isValid, Thumb, Track } from "./utils"
 interface IProps { min: number, max: number, price: number, setPrice: any, currency: string }
 
 
 
 export const Slider = ({ min, max, price, setPrice, currency }: IProps) => {
   const [writted, setWritted] = useState(`${price}`);
-  const handleChangeLeft = (value: number | readonly number[]): void => {
+  const handleInput = (e: any) => {
+    const value = e.target.value.slice(1);
+    if (value <= min && isValid(value)) { setPrice(min) };
+    if (value >= max && isValid(value)) { setPrice(max) };
+    if (value > min && value < max ) { setPrice(value) };
+
+
+    if (+value > max) { setWritted(String(max)); }
+    else
+      if (+value < min) { setWritted(String(value)); }
+      else {
+        setWritted(value);
+      }
+
+
+  }
+
+  const handleSlider = (value: number | readonly number[]): void => {
     setWritted(String(value))
     if (typeof value === 'number') {
       setWritted(String((value / 100) * (max - min) + min))
@@ -15,28 +32,21 @@ export const Slider = ({ min, max, price, setPrice, currency }: IProps) => {
     }
   }
 
-  const handleInput = (e: any) => {
-    const value = e.target.value.slice(1);
-    if (+value > max) { setWritted(String(max)); }
-    else
-      if (+value < min) { setWritted(String(min)); }
-      else {
-        setWritted(value);
-      }
-  }
-        return (
-          <S.Wrapper>
-            <S.Input
-              onChange={handleInput}
-              value={`${currency}${writted}`} price={(price - min) / (max - min) * 100} />
-            <S.Slider
-              value={(price - min) / (max - min) * 100}
-              onChange={handleChangeLeft}
-              renderTrack={Track}
-              renderThumb={Thumb} />
-            <S.Warning hidden >PLease, write a valid number</S.Warning>
-          </S.Wrapper >
+  console.log("writted", writted);
+  console.log("price", price);
+  return (
+    <S.Wrapper>
+      <S.Input
+        onChange={handleInput}
+        value={`${currency}${writted}`} price={(price - min) / (max - min) * 100} />
+      <S.Slider
+        value={(price - min) / (max - min) * 100}
+        onChange={handleSlider}
+        renderTrack={Track}
+        renderThumb={Thumb} />
+      <S.Warning hidden >PLease, write a valid number</S.Warning>
+    </S.Wrapper >
 
-        );
-      }
-  
+  );
+}
+
